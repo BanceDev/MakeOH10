d5c64c7c9dff8614eccd8c2f5570d1b67d56337b
BanceDev <lanceaborden@gmail.com>
2024-03-23 14:57:58 -0400

    made template

--- sketches/BlinkCount.ino
+++ sketches/BlinkCount.ino
@@ -1,16 +1,33 @@
 //BlinkCount
-void setup() {}
+#define RED 2
+#define YELLOW 3
+#define GREEN 4
+#define BLUE 5
+#define BUZZER 6
+void setup() {
+pinMode(RED, OUTPUT);
+pinMode(YELLOW, OUTPUT);
+pinMode(GREEN, OUTPUT);
+pinMode(BLUE, OUTPUT);
+pinMode(BUZZER,OUTPUT);
+}
 void loop() {
 for (int i = 0; i < 10; i++) {
+digitalWrite(GREEN, HIGH);
 delay(1);
-delay(1);
-}
-for (int i = 0; i < 10; i++) {
-delay(1);
+digitalWrite(GREEN, LOW);
 delay(1);
 }
 for (int i = 0; i < 10; i++) {
+digitalWrite(RED, HIGH);
 delay(1);
+digitalWrite(RED, LOW);
+delay(1);
+}
+for (int i = 0; i < 10; i++) {
+digitalWrite(BLUE, HIGH);
+delay(1);
+digitalWrite(BLUE, LOW);
 delay(1);
 }
 }
---
+++ tests/template.ino
@@ -0,0 +1,14 @@
+#define RED 2
+#define YELLOW 3
+#define GREEN 4
+#define BLUE 5
+#define BUZZER 6
+
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    pinMode(BUZZER,OUTPUT);
+}
